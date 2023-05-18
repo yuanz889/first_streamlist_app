2d20658fcd2d694699ac1c4226024337d7f13821
yuanz889 <133955775+yuanz889@users.noreply.github.com>
2023-05-18 15:28:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Mom\'s new healthy dinner')
 streamlit.header('Breakfast Menu')
@@ -8,3 +10,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
