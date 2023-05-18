5f08fa9d01e290b09ea24ffc4605ee97da4f5134
yuanz889 <133955775+yuanz889@users.noreply.github.com>
2023-05-18 19:42:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruits_to_show )
 # streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
