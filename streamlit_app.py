7ca33edd60c3bf15f13380c482e7371611b4556d
yuanz889 <133955775+yuanz889@users.noreply.github.com>
2023-05-18 15:27:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom''s new healthy dinner')
+streamlit.title('My Mom\'s new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
